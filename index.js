42bc7f719a68dbd4751a6b9a7a025d3d43bf7976
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-04-23 18:32:06 -0400

    Firebase DB URL added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const appSettings = {
+    databaseURL:"https://playground-firebase-914-default-rtdb.firebaseio.com/"
+}
+
 const inputFieldEl = document.getElementById('input-field')
 const addButtonEl = document.getElementById('add-button')
 
